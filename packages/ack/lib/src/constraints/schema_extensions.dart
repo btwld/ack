af70b35774f762a32b9c74a50262c101f92e4795
Leo Farias <leoafarias@gmail.com>
2025-06-18 15:27:22 -0400

    feat: prepare for 0.3.0-beta.1 release (#11)

--- packages/ack/lib/src/constraints/schema_extensions.dart
+++ packages/ack/lib/src/constraints/schema_extensions.dart
@@ -1,4 +1,6 @@
 /// Extension methods for all schema types to provide additional validation capabilities.
 extension SchemaExtensions on Object {
   // Future extensions will be added here
+  // Note: preprocess() method will be implemented in a future release
+  // when the architecture supports it properly
 }
