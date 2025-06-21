0e7f19fa9413ee853ad4955a2e283852b681f0c1
Leo Farias <leoafarias@gmail.com>
2025-06-21 15:02:33 -0400

    step 3

---
+++ packages/ack_generator/lib/ack_generator.dart
@@ -0,0 +1,8 @@
+/// Code generation for Ack schema validation models.
+library ack_generator;
+
+// Export the builder for build.yaml
+export 'src/builder.dart' show ackGenerator;
+
+// TODO: Export annotations from ack_annotations when package is created
+// export 'package:ack_annotations/ack_annotations.dart';
